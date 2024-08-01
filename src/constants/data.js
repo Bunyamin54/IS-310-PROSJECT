87eca4f2d5c49658dc1028e901bcd188eb92074c
“Bunyamin54”~ <bunyaming@uia.no>
2024-08-01 12:07:37 +0200

    exempel data added

--- src/constants/data.js
+++ src/constants/data.js
@@ -14,6 +14,76 @@ export const PROJECTS = [
 
 
 },
+{
+  title : "Appointment App:",
+  image: project1,
+  description: "React, Bootstrap og JavaScript ble brukt i dette prosjektet. Både komponentstrukturen og modal CSSble benyttet. Brukeren fikk mulighet til å velge ønsket dag og klokkeslett hos legen han valgte, samt bruke funksjonene for å bestille time og avbestille.",
+  technologies: ["React, JavaScript, CSS, Bootstrap "]
+
+
+
+
+},
+{
+  title : "Appointment App:",
+  image: project1,
+  description: "React, Bootstrap og JavaScript ble brukt i dette prosjektet. Både komponentstrukturen og modal CSSble benyttet. Brukeren fikk mulighet til å velge ønsket dag og klokkeslett hos legen han valgte, samt bruke funksjonene for å bestille time og avbestille.",
+  technologies: ["React, JavaScript, CSS, Bootstrap "]
+
+
+
+
+},
+{
+  title : "Appointment App:",
+  image: project1,
+  description: "React, Bootstrap og JavaScript ble brukt i dette prosjektet. Både komponentstrukturen og modal CSSble benyttet. Brukeren fikk mulighet til å velge ønsket dag og klokkeslett hos legen han valgte, samt bruke funksjonene for å bestille time og avbestille.",
+  technologies: ["React, JavaScript, CSS, Bootstrap "]
+
+
+
+
+},
+{
+  title : "Appointment App:",
+  image: project1,
+  description: "React, Bootstrap og JavaScript ble brukt i dette prosjektet. Både komponentstrukturen og modal CSSble benyttet. Brukeren fikk mulighet til å velge ønsket dag og klokkeslett hos legen han valgte, samt bruke funksjonene for å bestille time og avbestille.",
+  technologies: ["React, JavaScript, CSS, Bootstrap "]
+
+
+
+
+},
+{
+  title : "Appointment App:",
+  image: project1,
+  description: "React, Bootstrap og JavaScript ble brukt i dette prosjektet. Både komponentstrukturen og modal CSSble benyttet. Brukeren fikk mulighet til å velge ønsket dag og klokkeslett hos legen han valgte, samt bruke funksjonene for å bestille time og avbestille.",
+  technologies: ["React, JavaScript, CSS, Bootstrap "]
+
+
+
+
+},
+{
+  title : "Appointment App:",
+  image: project1,
+  description: "React, Bootstrap og JavaScript ble brukt i dette prosjektet. Både komponentstrukturen og modal CSSble benyttet. Brukeren fikk mulighet til å velge ønsket dag og klokkeslett hos legen han valgte, samt bruke funksjonene for å bestille time og avbestille.",
+  technologies: ["React, JavaScript, CSS, Bootstrap "]
+
+
+
+
+},
+{
+  title : "Appointment App:",
+  image: project1,
+  description: "React, Bootstrap og JavaScript ble brukt i dette prosjektet. Både komponentstrukturen og modal CSSble benyttet. Brukeren fikk mulighet til å velge ønsket dag og klokkeslett hos legen han valgte, samt bruke funksjonene for å bestille time og avbestille.",
+  technologies: ["React, JavaScript, CSS, Bootstrap "]
+
+
+
+
+},
 
 
 
